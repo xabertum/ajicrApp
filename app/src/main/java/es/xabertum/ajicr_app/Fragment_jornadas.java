1e7519b2317be977827d818b5fe16ceb3f2ba15d
xabertum <mohatar@gmail.com>
2019-03-11 11:05:06 +0100

    Added files

---
+++ app/src/main/java/es/xabertum/ajicr_app/Fragment_jornadas.java
@@ -0,0 +1,16 @@
+package es.xabertum.ajicr_app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_jornadas extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ) {
+        return inflater.inflate(R.layout.fragment_jornadas, null);
+    }
+}
--- app/src/main/java/es/xabertum/ajicr_app/MainActivity.java
+++ app/src/main/java/es/xabertum/ajicr_app/MainActivity.java
@@ -2,53 +2,55 @@ package es.xabertum.ajicr_app;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-    private TextView contentHome;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    contentHome.setText(R.string.content_home);
-                    return true;
-                case R.id.navigation_jornadas:
-                    mTextMessage.setText(R.string.title_jornadas);
-                    return true;
-                case R.id.navigation_encuentros
-                        :
-                    mTextMessage.setText(R.string.title_encuentros);
-                    return true;
-                case R.id.navigation_contacto:
-                    mTextMessage.setText(R.string.title_contacto);
-                    return true;
-            }
-            return false;
-        }
-    };
+public class MainActivity extends AppCompatActivity  implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_inicio);
 
-        contentHome = (TextView) findViewById(R.id.content_home);
+        //loading the default fragment
+        loadFragment(new Fragment_Inicio());
+
         //contentHome.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
 
-        mTextMessage = (TextView) findViewById(R.id.content_home);
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new Fragment_Inicio();
+                break;
+
+            case R.id.navigation_jornadas:
+                fragment = new Fragment_jornadas();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment (Fragment fragment) {
+        // switching frament
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+
+        return false;
     }
 
 }
