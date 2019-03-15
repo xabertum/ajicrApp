e4c2c7a212d693a128bda986708bd2e3e83d4002
xabertum <mohatar@gmail.com>
2019-03-15 18:55:52 +0100

    Added layouts

--- app/src/main/java/es/xabertum/ajicr_app/Fragment_jornadas_v.java
+++ app/src/main/java/es/xabertum/ajicr_app/Fragment_jornadas_v.java
@@ -1,32 +1,56 @@
 package es.xabertum.ajicr_app;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class Fragment_jornadas_v extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Fragment_jornadas_v extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mapView;
+    View jornadasV;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ) {
 
-        View jornadasV = inflater.inflate(R.layout.fragment_jornadas_v, null);
-
-
-
-
-
+        jornadasV = inflater.inflate(R.layout.fragment_jornadas_v, null);
         return jornadasV;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        mapView = (MapView) jornadasV.findViewById(R.id.mapView);
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(40.448837, -3.730364)).title("Facultad de Filosofía UCM"));
+        CameraPosition facultadFilosofia = CameraPosition.builder().target(new LatLng(40.448837, -3.730364)).zoom(15).bearing(0).tilt(45).build();
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(facultadFilosofia));
 
-
-
-
+    }
 }
