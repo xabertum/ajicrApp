e8d8cbed3a6d321c7a2409e5e56fcdda03bbee37
xabertum <mohatar@gmail.com>
2019-03-21 20:50:09 +0100

    Added Glide library

--- app/src/main/java/es/xabertum/ajicr_app/Fragment_jornadas_1.java
+++ app/src/main/java/es/xabertum/ajicr_app/Fragment_jornadas_1.java
@@ -7,16 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Fragment_jornadas_1 extends Fragment {
 
     View jornadas_1;
+    ImageView imageViewCartel;
+    ImageView imageViewPortada;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ) {
 
         jornadas_1 = inflater.inflate(R.layout.fragment_jornadas_1,null);
+        imageViewCartel = jornadas_1.findViewById(R.id.cartel_jornadas);
+        imageViewPortada = jornadas_1.findViewById(R.id.imageViewPortada1);
+
+        Glide.with(this).load(R.drawable.cartel_jornadas_1).into(imageViewCartel);
+        Glide.with(this).load(R.drawable.portada_j1).into(imageViewPortada);
 
         return jornadas_1;
     }
